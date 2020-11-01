1fb5571c21aea69a5a6167d29d467f5cdb6ff511
Erik Hals <erikhals@gmail.com>
2020-11-01 10:23:56 +0100

    Add serializer for mux

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,12 +1,12 @@
 import Figure from "./figure";
 import Youtube from "./youtube";
-import Video from "./muxvideo";
+import Video from "sanity-mux-player";
 
 const serializers = {
   types: {
     figure: Figure,
     youtube: Youtube,
-    video: Video
+    "mux.video": Video
   }
 };
 
