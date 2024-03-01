e4d262c749976715b3dbca879fd852ea696599d7
Erik Hals <erikhals@gmail.com>
2024-03-01 10:14:24 +0100

    Connect Sanity and Astro

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,4 +1,16 @@
+import {sanityIntegration} from "@sanity/astro"
 import { defineConfig } from 'astro/config';
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanityIntegration({
+      projectId: 'oxkz69uk',
+      dataset: 'production',
+      apiVersion: '2023-02-08',
+      useCdn: false,
+    })
+  ]
+});
---
+++ web/utils/sanity.ts
@@ -0,0 +1,29 @@
+import { sanityClient } from "sanity:client";
+import type { PortableTextBlock } from "@portabletext/types";
+import type { ImageAsset, Slug } from "@sanity/types";
+import groq from "groq";
+
+export async function getProjects(): Promise<Project[]> {
+  return await sanityClient.fetch(
+    groq`*[_type == "project" && defined(slug.current)] | order(_createdAt desc)`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  return await sanityClient.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]`,
+    {
+      slug,
+    }
+  );
+}
+
+export interface Project {
+  _type: "project";
+  _createdAt: string;
+  title?: string;
+  slug: Slug;
+  excerpt?: string;
+  mainImage?: ImageAsset;
+  body: PortableTextBlock[];
+}
